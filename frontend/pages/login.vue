7cc906920b956572c2df9ad9189278ca649abcc3
Stew <ghost.pizza@gmail.com>
2025-11-07 21:58:31 +0100

    style update

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -7,14 +7,14 @@
         v-model="email"
         type="email"
         placeholder="Email"
-        class="w-full border rounded p-2"
+       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         required
       />
       <input
         v-model="password"
         type="password"
         placeholder="Password"
-        class="w-full border rounded p-2"
+       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         required
       />
       <button
--- frontend/pages/register.vue
+++ frontend/pages/register.vue
@@ -8,7 +8,7 @@
         <input
           v-model="email"
           type="email"
-          class="w-full border rounded p-2"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           required
         />
       </div>
@@ -17,7 +17,7 @@
         <input
           v-model="username"
           type="text"
-          class="w-full border rounded p-2"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           required
         />
       </div>
@@ -26,7 +26,7 @@
         <input
           v-model="password"
           type="password"
-          class="w-full border rounded p-2"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           required
         />
       </div>
