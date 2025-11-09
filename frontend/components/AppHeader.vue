48c0b5e07cf809ef20286f22ebe0a28cfd8274d9
Stew <ghost.pizza@gmail.com>
2025-11-09 09:32:45 +0100

    Update AppHeader.vue

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -45,7 +45,7 @@
                 class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-100 md:dark:hover:text-blue-400 dark:hover:bg-gray-800 md:dark:hover:bg-transparent"
                 @click="closeOnMobile"
               >
-                Login
+                Accedi
               </NuxtLink>
             </li>
             <li>
@@ -54,7 +54,7 @@
                 class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-100 md:dark:hover:text-blue-400 dark:hover:bg-gray-800 md:dark:hover:bg-transparent"
                 @click="closeOnMobile"
               >
-                Register
+                Inizia gratis
               </NuxtLink>
             </li>
           </template>
