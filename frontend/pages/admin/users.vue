299492bf4c1f055ed574b41e354848c76a63ef98
Stew <ghost.pizza@gmail.com>
2025-11-21 16:11:43 +0100

    Update users.vue

--- frontend/pages/admin/users.vue
+++ frontend/pages/admin/users.vue
@@ -19,19 +19,21 @@
           :disabled="backfillRunning"
         >
           <svg
-            v-if="!backfillRunning"
-            class="w-4 h-4"
-            viewBox="0 0 20 20"
-            fill="none"
-          >
-            <path
-              d="M4 4v4h4M4 4l3-3M16 16v-4h-4m4 4-3 3"
-              stroke="currentColor"
-              stroke-width="1.6"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
+    v-if="!backfillRunning"
+    class="w-5 h-5 text-white"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"
+    />
+  </svg>
           <svg
             v-else
             class="animate-spin h-4 w-4"
